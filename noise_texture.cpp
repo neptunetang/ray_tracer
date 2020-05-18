61c7c4d4972019084bb479d1aeb8e3bb926e7523
neptunetang <neptunetangruixin@gmail.com>
2020-05-18 12:12:02 +0200

    rerange

---
+++ noise_texture.cpp
@@ -0,0 +1,9 @@
+//
+// Created by neptune on 18-05-20.
+//
+
+#include "noise_texture.h"
+
+vec3 noise_texture::value(float u, float v, const vec3 &p) const {
+    return vec3(1,1,1)*noise.noise(p*scale);
+}
--- noise_texture.h
+++ noise_texture.h
@@ -12,9 +12,7 @@ class noise_texture :public texture{
 public:
     noise_texture() = default;
     noise_texture(float s) : scale(s){}
-    virtual vec3 value (float u, float v, const vec3& p)const{
-        return vec3(1,1,1)*noise.noise(p*scale);
-    }
+    virtual vec3 value (float u, float v, const vec3& p)const;
 
 private:
     perlin noise;
