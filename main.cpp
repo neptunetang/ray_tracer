de78cf9cb24cf4106d6d9b6a5e899ee396c8726a
neptunetang <neptunetangruixin@gmail.com>
2020-05-24 17:58:17 +0200

    multithreading works

--- main.cpp
+++ main.cpp
@@ -22,7 +22,28 @@
 #include "light.h"
 #include "light_list.h"
 #include <iostream>
-#include<fstream>
+#include <fstream>
+
+#include <chrono>
+#include <string>
+
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+std::mutex writeM;
+
+struct BlockJob
+{
+    int rowStart;
+    int rowEnd;
+    int colSize;
+    int spp;
+    std::vector<int> indices;
+    std::vector<vec3> colors;
+};
 
 vec3 nne(const hit_record rec, const hitable* world, light light_source, ray r_in){
     vec3 emitted = vec3(0,0,0);
@@ -67,15 +88,56 @@ vec3 color(const ray& r, const vec3& background, const hitable* world, int depth
     return background;
 }
 
+void CalculateColor(BlockJob job, std::vector<BlockJob>& imageBlocks, int height, camera cam, hitable* world, light light_source,
+                    std::mutex& mutex, std::condition_variable& cv, std::atomic<int>& completedThreads)
+{
+    int max_depth = 4;
+    for (int j = job.rowStart; j < job.rowEnd; ++j) {
+        for (int i = 0; i < job.colSize; ++i) {
+            vec3 col(0, 0, 0);
+            int useful_sample = 0;
+            for (int s = 0; s < job.spp; ++s) {
+                int root = int(sqrt(job.spp));
+                auto stratum_x = s%root/root;
+                auto stratum_y = s/job.spp;
+                auto u = double(i+stratum_x+random_float()*0.25) / float(job.colSize);
+                auto v = double(j+stratum_y+random_float()*0.25) / height;
+                ray r = cam.get_ray(u, v);
+                vec3 c = color(r, vec3(0,0,0), world, max_depth, light_source, 'n');
+                if(isnan(c.r())||isnan(c.g())||isnan(c.b())){
+                    continue;
+                }
+                col += c;
+                useful_sample++;
+            }
+
+            col /= float(useful_sample);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+            const unsigned int index = (height-j-1) * job.colSize + i;
+            job.indices.push_back(index);
+            job.colors.push_back(col);
+        }
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        imageBlocks.push_back(job);
+        completedThreads++;
+        cv.notify_one();
+    }
+}
+
 void run(int scene){
     int width=400, height=400;
-    int sample_per_pixel = 20;
-    int max_depth = 4;
-    ofstream img ("y.ppm");
+    int sample_per_pixel = 16;
+    int pixelCount = width * height;
+    ofstream img ("boxlight_ptnne.ppm");
     img << "P3" << endl;
     img << width << " " << height << endl;
     img << "255" << endl;
-    const vec3 background(0,0,0);
+    vec3* image = new vec3[pixelCount];
+    memset(&image[0], 0, pixelCount * sizeof(vec3));
 
     material* white = new diffuse(new constant_texture(vec3(1,1,1)));
 
@@ -184,8 +246,14 @@ void run(int scene){
             //list[i++] = new xz_rect(0,555,0,555,201, white);
             list[i++] = new xy_rect(0,555,0,555,555, white);
 
-            list[i++] = new sphere(vec3(275,275,275), 10, new diffuse_light(new constant_texture(vec3(10,10,10))));
-            light_area = light(new sphere (vec3(275,275,275), 10, new diffuse_light(new constant_texture(vec3(10,10,10)))),
+            list[i++] = new flip_face(new yz_rect(200,350,200,350,200, white));
+            list[i++] = new yz_rect(200,350,200,350,350, white);
+            list[i++] = new xy_rect(200,350,200,350,200, white);
+            list[i++] = new flip_face(new xy_rect(200,350,200,350,350, white));
+            list[i++] = new flip_face(new xz_rect(200,350,200,350,350, white));
+
+            list[i++] = new sphere(vec3(275,275,275), 50, new diffuse_light(new constant_texture(vec3(10,10,10))));
+            light_area = light(new sphere (vec3(275,275,275), 50, new diffuse_light(new constant_texture(vec3(10,10,10)))),
                                vec3(10,10,10), 10);
             //list[i++] = new xz_rect(213,343,227,332,554,new diffuse_light(new constant_texture(vec3(5,5,5)),10));
 
@@ -193,8 +261,10 @@ void run(int scene){
 //            box1 = new rotate_y(box1, 15);
 //            list[i++] = new translate(box1, vec3(265,0,295));
 
-            list[i++] = new sphere(vec3(300,70,300),70, new metal(new constant_texture(vec3(1,1,1)),0.0));
+            //list[i++] = new sphere(vec3(300,70,300),70, new metal(new constant_texture(vec3(1,1,1)),0.0));
             list[i++] = new sphere(vec3(200,70,200),70, white);
+
+            //list[i++] = new sphere(vec3(300,100,300), 100, new dielectric(1.5));
 
 //            box2 = new box(vec3(0,0,0), vec3(165,100,165), white);
 //            box2 = new rotate_y(box2, -18);
@@ -298,32 +368,74 @@ void run(int scene){
     camera cam(look_from, look_at, vup, 40, (width/height), aperture, focus,0.0,1.0);
 
 
-    for (int j = height-1; j >= 0; j--) {
-        for (int i = 0; i < width; i++) {
-            vec3 col(0,0,0);
-            int useful_sample = 0;
-            for(int s = 0; s < sample_per_pixel; s++){
-                auto u = double(i+random_float()) / width;
-                auto v = double(j+random_float()) / height;
-                ray r2 = cam.get_ray(u,v);
-                vec3 c = color(r2, background, world, max_depth, light_area, 'n');
-                if(isnan(c.r())||isnan(c.g())||isnan(c.b())){
-                    continue;
-                }
-                col += c;
-                useful_sample++;
-            }
+    auto fulltime = std::chrono::high_resolution_clock::now();
 
-            col /= float(useful_sample);
-            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+    const int nThreads = std::thread::hardware_concurrency();
+    int rowsPerThread = height / nThreads;
+    int leftOver = height % nThreads;
 
-            int ir = static_cast<int>(255.999 * col[0]);
-            int ig = static_cast<int>(255.999 * col[1]);
-            int ib = static_cast<int>(255.999 * col[2]);
-            img << ir << ' ' << ig << ' ' << ib << endl;
+    std::mutex mutex;
+    std::condition_variable cvResults;
+    std::vector<BlockJob> imageBlocks;
+    std::atomic<int> completedThreads = { 0 };
+    std::vector<std::thread> threads;
 
+    for (int i = 0; i < nThreads; ++i)
+    {
+        BlockJob job;
+        job.rowStart = i * rowsPerThread;
+        job.rowEnd = job.rowStart + rowsPerThread;
+        if (i == nThreads - 1)
+        {
+            job.rowEnd = job.rowStart + rowsPerThread + leftOver;
+        }
+        job.colSize = width;
+        job.spp = sample_per_pixel;
+
+        std::thread t([job, &imageBlocks, height, &cam, &world, &light_area, &mutex, &cvResults, &completedThreads]() {
+            CalculateColor(job, imageBlocks, height, cam, world, light_area, mutex, cvResults, completedThreads);
+        });
+        threads.push_back(std::move(t));
+    }
+
+
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cvResults.wait(lock, [&completedThreads, &nThreads] {
+            return completedThreads == nThreads;
+        });
+    }
+
+    for (std::thread& t : threads){
+        t.join();
+    }
+
+    for (BlockJob job : imageBlocks){
+        int index = job.rowStart;
+        int colorIndex = 0;
+        for (vec3& col : job.colors)
+        {
+            int colIndex = job.indices[colorIndex];
+            image[colIndex] = col;
+            ++colorIndex;
         }
     }
+
+
+
+    for (unsigned int i = 0; i < width*height; i++){
+
+        img
+                << static_cast<int>(255.99f * image[i].e[0]) << " "
+                << static_cast<int>(255.99f * image[i].e[1]) << " "
+                << static_cast<int>(255.99f * image[i].e[2]) << "\n";
+    }
+    auto timeSpan = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - fulltime);
+    int frameTimeMs = static_cast<int>(timeSpan.count());
+    std::cout << " - time " << frameTimeMs << " ms \n";
+    std::cout << "File Saved" << std::endl;
+    delete[] image;
+
 }
 
 int main() {
